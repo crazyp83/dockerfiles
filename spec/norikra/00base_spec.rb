9dfbade7aca5058db0b90b70cf962d0e61e3de8f
crazyp83 <crazyp@icloud.com>
2025-01-24 17:10:05 -0500

    Update 00base_spec.rb

--- spec/norikra/00base_spec.rb
+++ spec/norikra/00base_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe 'minimum2scp/norikra' do
+describe 'crazyp83/norikra' do
   context 'without env' do
     before(:all) do
       start_container({
-        'Image' => ENV['DOCKER_IMAGE'] || "minimum2scp/#{File.basename(__dir__)}:latest",
+        'Image' => ENV['DOCKER_IMAGE'] || "crazyp83/#{File.basename(__dir__)}:latest",
       })
     end
 
