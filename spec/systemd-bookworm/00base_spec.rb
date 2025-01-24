ab52113eda2d73bfdb0ee4353363def5bb8a4f1a
crazyp83 <crazyp@icloud.com>
2025-01-24 17:12:57 -0500

    Update 00base_spec.rb

--- spec/systemd-bookworm/00base_spec.rb
+++ spec/systemd-bookworm/00base_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe 'minimum2scp/systemd-bookworm' do
+describe 'crazyp83/systemd-bookworm' do
   context 'without env' do
     before(:all) do
       start_container({
-        'Image' => ENV['DOCKER_IMAGE'] || "minimum2scp/#{File.basename(__dir__)}:latest",
+        'Image' => ENV['DOCKER_IMAGE'] || "crazyp83/#{File.basename(__dir__)}:latest",
         'HostConfig' => { 'Privileged' => true },
       })
     end
