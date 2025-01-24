1f2da8f6f0cba22bc90f12e42d36a692422c0cf2
crazyp83 <crazyp@icloud.com>
2025-01-24 17:06:07 -0500

    Update 01apt_spec.rb

--- spec/baseimage-bookworm/01apt_spec.rb
+++ spec/baseimage-bookworm/01apt_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe 'minimum2scp/baseimage-bookworm' do
+describe 'crazyp83/baseimage-bookworm' do
   describe 'apt' do
     before(:all) do
       start_container({
-        'Image' => ENV['DOCKER_IMAGE'] || "minimum2scp/#{File.basename(__dir__)}:latest",
+        'Image' => ENV['DOCKER_IMAGE'] || "crazyp83/#{File.basename(__dir__)}:latest",
       })
     end
 
